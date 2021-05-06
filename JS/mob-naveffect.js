84586266448c7b9a30d5a9d57a764007277e8ae0
KingDavid237 <83729150+KingDavid237@users.noreply.github.com>
2021-05-05 23:39:49 -0700

    My Second Commit
    Crimson Tech Web

---
+++ JS/mob-naveffect.js
@@ -0,0 +1,16 @@
+const navSlide = () => {
+	const burger = document.querySelector('.burger');
+	const nav = document.querySelector('.nav-links');
+	const navLinks = document.querySelectorAll('.nav-links li');
+
+	burger.addEventListener('click', () => {
+		nav.classList.toggle('nav-active');
+	}); 
+
+	navLinks.forEach((link, index) => {
+		link.style.animation = 'navLinks 0.5s ease forwards ${index / 7	+ 2}s';
+		// console.log(index / 7);
+	});
+};
+
+navSlide();
